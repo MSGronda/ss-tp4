016c50cc75d5e5f946ff7b931073b95910cd6b12
MSGronda <mgronda@itba.edu.ar>
2024-05-01 15:05:02 -0300

    feat: animate in parallel

--- python/ej2/src/animation.py
+++ python/ej2/src/animation.py
@@ -1,4 +1,5 @@
-from multiprocessing import Pool
+import os
+from multiprocessing import Pool, Manager, Process
 
 import cv2
 import numpy as np
@@ -9,8 +10,8 @@ def generate_center(body, plane_scale, offset):
     return int((body[X] + offset) * plane_scale), int((body[Y] + offset) * plane_scale)
 
 
-def animate(body_data: [], properties: {}, video_width: int):
-    fps = 120
+def animate(filename: str, body_data: [], properties: {}, video_width: int, starting: int = 0, ending: int = -1):
+    fps = 30
 
     width, height = calc_plane_dimensions(body_data)
     offset = width
@@ -18,29 +19,29 @@ def animate(body_data: [], properties: {}, video_width: int):
     plane_scale = video_width / (2 * width)
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('../animations/simulation_video.mp4', fourcc, fps, (video_width, video_width))
+    out = cv2.VideoWriter(filename, fourcc, fps, (video_width, video_width))
 
     # Sun
-    sun_r = max(10, int(100 * plane_scale))  # TODO: change
+    sun_r = max(10, int(properties["SUN"]["r"] * plane_scale))  # TODO: change
     sun_color = (0, 255, 255)
     sun_pos = int(width * plane_scale)
 
     # Earth
-    earth_r = max(3, int(25 * plane_scale))
+    earth_r = max(3, int(properties["EARTH"]["r"] * plane_scale))
     earth_idx = properties["EARTH"]["index"]
     earth_color = (255, 0, 0)
 
     # Mars
-    mars_r = max(2, int(10 * plane_scale))
+    mars_r = max(2, int(properties["MARS"]["r"] * plane_scale))
     mars_idx = properties["MARS"]["index"]
     mars_color = (0, 0, 255)
 
     # Spaceship
-    spaceship_r = max(2, int(5 * plane_scale))
+    spaceship_r = 2
     spaceship_idx = properties["SPACESHIP"]["index"]
     spaceship_color = (0, 255, 0)
 
-    for bodies in body_data:
+    for bodies in body_data[starting:ending]:
         frame = np.ones((video_width, video_width, 3), dtype=np.uint8) * 255
 
         # Sun
@@ -58,3 +59,48 @@ def animate(body_data: [], properties: {}, video_width: int):
         out.write(frame)
 
     out.release()
+
+
+def animate_for_parallel(args):
+    animate(*args)
+
+
+def combine_videos(input_files, output_file, screen_size: int):
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    output_video = cv2.VideoWriter(output_file, fourcc, 30.0, (screen_size, screen_size))
+
+    for file in input_files:
+        input_video = cv2.VideoCapture(file)
+
+        while True:
+            ret, frame = input_video.read()
+            if not ret:
+                break
+            output_video.write(frame)
+
+        input_video.release()
+
+    output_video.release()
+
+
+def animate_parallel(body_data: [], properties: {}, video_width: int, num_process: int):
+    frames_per_process = int(len(body_data) / num_process)
+
+    args = []
+    for i in range(num_process):
+        args.append((f"../animations/simulation-video-{i}.mp4", body_data, properties, video_width, i * frames_per_process, (i+1) * frames_per_process))
+
+    with Pool() as pool:
+        pool.map(animate_for_parallel, args)
+        pool.close()
+        pool.join()
+
+    combination_files = [arg[0] for arg in args]
+
+    combine_videos(combination_files, "../animations/simulation-video.mp4", video_width)
+
+    for arg in args:
+        os.remove(arg[0])
+
+
+
--- python/ej2/src/main.py
+++ python/ej2/src/main.py
@@ -1,8 +1,9 @@
-from ej2.src.animation import animate
+from ej2.src.animation import animate, animate_parallel
 from utils import *
 
 if __name__ == "__main__":
-    time, body_data = get_body_data(get_all_files()[-1])
-    properties = get_static_data(get_all_static_files()[-1])
+    time, body_data = get_body_data(get_all_files()[0])
+    properties = get_static_data(get_all_static_files()[0])
 
-    animate(body_data, properties, 1080)
+    animate_parallel(body_data, properties, 1080, 8)
+
